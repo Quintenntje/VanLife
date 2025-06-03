bb27886168adb030ca13f88622e1dde4656e3be8
Quinten <quintenclaes7@gmail.com>
2025-06-03 19:25:53 +0200

    Refactor App and Navbar components to implement routing and navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/about" element={<h1>About</h1>} />
-        </Routes>
-      </BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">#Van-life</Link>
+        </li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
