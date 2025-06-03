773667c0a9cdb1020500c9ac7d4218c010f0d1d1
Quinten <quintenclaes7@gmail.com>
2025-06-03 23:07:37 +0200

    fix: correct active class assignment in Tag component for proper styling

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -6,13 +6,13 @@ export default function Tag({ children, isActive }) {
   const tagClass = (() => {
     switch (children.toLowerCase()) {
       case "simple":
-        activeClass = "bg-gray-100 text-gray-800";
+        isActive ? (activeClass = "bg-gray-100 text-gray-800") : "";
         return "hover:bg-gray-100 hover:text-gray-800";
       case "luxury":
-        activeClass = "bg-yellow-100 text-yellow-800";
+        isActive ? (activeClass = "bg-yellow-100 text-yellow-800") : "";
         return "hover:bg-yellow-100 hover:text-yellow-800";
       case "rugged":
-        activeClass = "bg-green-100 text-green-800";
+        isActive ? (activeClass = "bg-green-100 text-green-800") : "";
         return "hover:bg-green-100 hover:text-green-800";
       default:
         return;
@@ -20,9 +20,7 @@ export default function Tag({ children, isActive }) {
   })();
 
   return (
-    <span
-      className={`${defaultClasses} ${tagClass} ${isActive ? activeClass : ""}`}
-    >
+    <span className={`${defaultClasses} ${tagClass} ${activeClass}`}>
       {children}
     </span>
   );
