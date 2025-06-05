cdfada5ae96940f59b04028e5416867441a61881
Quinten <quintenclaes7@gmail.com>
2025-06-05 16:17:24 +0200

    fix: correct case sensitivity in Tag component and improve formatting in VanCard component

--- src/components/VanCard.jsx
+++ src/components/VanCard.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Tag from "./Tag";
 export default function VanCard({ van }) {
   return (
-    <article >
+    <article>
       <img className="rounded-xl" src={van.imageUrl} alt={van.name} />
       <div className="flex justify-between items-center">
-        <h3>{van.name}</h3>
-        <p className="block text-2xl"> <span className="text-gray-700">${van.price}</span> <span className="text-gray-300 text-sm">/day</span></p>
+        <h3 className="text-2xl">{van.name}</h3>
+        <p className="flex flex-col text-2xl">
+          <span className="text-gray-700">${van.price}</span>
+          <span className="text-gray-500 text-sm">/day</span>
+        </p>
       </div>
-      <Tag isActive={true}>${van.type}</Tag>
+      <Tag isActive={true}>{van.type}</Tag>
     </article>
   );
 }
