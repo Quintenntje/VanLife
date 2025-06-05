a6828ce3bba6df2e6a0b68a9accd8021ee96c4f8
Quinten <quintenclaes7@gmail.com>
2025-06-05 18:02:41 +0200

    fix: nest routes under Layout component for better structure

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,17 @@ import "./server";
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
-
       <Routes>
-        <Route element={<Layout />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
-        <Route path="/host" element={<Dashboard />} />
-        <Route path="/host/income" element={<Income />} />
-        <Route path="/host/reviews" element={<Reviews />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />}>
+            <Route path="/income" element={<Income />} />
+            <Route path="/reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
