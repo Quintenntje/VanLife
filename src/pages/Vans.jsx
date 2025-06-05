a0cf91ba003b22b492d27ec7395f466143b2a033
Quinten <quintenclaes7@gmail.com>
2025-06-05 16:00:10 +0200

    feat: enhance Vans page layout with grid structure for better display

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,5 +1,13 @@
-export default function Vans(){
-    return (
-        <h1>Expore our vans</h1>
-    )
+import React from "react";
+import "../components/VanCard";
+export default function Vans() {
+  return (
+    <>
+      <h1>Expore our vans</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+         
+      </div>
+    </>
+  );
 }
