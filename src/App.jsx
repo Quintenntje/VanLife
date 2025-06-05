36cb3ed60ed304c69cf0e3ae621a81107b049d97
Quinten <quintenclaes7@gmail.com>
2025-06-05 18:19:32 +0200

    fix: update host routes to use relative paths for better routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -21,10 +21,10 @@ function App() {
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
 
-          <Route path="/host" element={<HostLayout />}>
+          <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
