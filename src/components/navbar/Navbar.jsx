2dc4a8384e299e70012d021eaa3ed205c7f9c96a
Quinten <quintenclaes7@gmail.com>
2025-06-05 18:46:58 +0200

    fix: replace Link with NavLink in Navbar and NavbarLink components for consistent routing behavior

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import NavbarLink from "./NavbarLink";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -6,7 +7,7 @@ export default function Navbar() {
       <nav className="max-w-6xl mx-auto px-4 py-5">
         <div className="flex justify-between items-center">
           <div className="font-bold text-xl md:text-2xl">
-            <NavbarLink to="/"> #VANLIFE</NavbarLink>
+            <NavLink to="/"> #VANLIFE</NavLink>
           </div>
           <div className="flex space-x-4 md:space-x-6">
             <NavbarLink to="/host">Host</NavbarLink>
--- src/components/navbar/NavbarLink.jsx
+++ src/components/navbar/NavbarLink.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export default function NavbarLink({ to, children }) {
   return (
-    <Link
+    <NavLink
+      end={children === "Dashboard"}
       to={to}
-      className="text-black hover:text-orange-500 transition-colors"
+      className={({isActive}) => isActive ? "text-orange-500" : "text-black hover:text-orange-500 transition-colors"}
     >
       {children}
-    </Link>
+    </NavLink>
   );
 }
