a264c20e0216e08490cda3835954ff2581426736
Quinten <quintenclaes7@gmail.com>
2025-06-12 18:42:04 +0200

    feat: refactor AuthForm and Login components for improved structure and functionality

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,9 +1,10 @@
 import Input from "../components/Input";
 
+
 export default function AuthForm({ title, onSubmit, buttonText, inputFields }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold mb-6">{title}</h1>
+    <div className="flex flex-col items-center justify-center mt-32">
+      <h1 className="text-3xl font-bold mb-6">{title}</h1>
       <form className="w-full max-w-sm space-y-4" onSubmit={onSubmit}>
         {inputFields.map((field) => (
           <Input
@@ -14,7 +15,7 @@ export default function AuthForm({ title, onSubmit, buttonText, inputFields }) {
         ))}
         <button
           type="submit"
-          className="w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition-colors"
+          className="cursor-pointer w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition-colors"
         >
           {buttonText}
         </button>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,25 @@
 import AuthForm from "../components/AuthForm";
+import { useState } from "react";
 
 export default function Login() {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleLoginSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    setLoginData({ email, password });
+    console.log("Login data submitted:", loginData);
+  }
+
   return (
     <AuthForm
       title="Login"
-      onSubmit={(e) => {
-        e.preventDefault();
-      }}
+      onSubmit={handleLoginSubmit}
       buttonText="Login"
       inputFields={[
         { id: "email", type: "email", placeholder: "Email" },
