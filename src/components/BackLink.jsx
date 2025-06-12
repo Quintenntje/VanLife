357f0fc1cb40bb31447bf2407763d0bea171af0c
Quinten <quintenclaes7@gmail.com>
2025-06-12 17:57:20 +0200

    feat: add BackLink component and include react-icons dependency

---
+++ src/components/BackLink.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiArrowLeft } from "react-icons/hi2";
+export default function BackLink({ to, children }) {
+  return (
+    <Link
+      to={to}
+      className="flex items-center gap-2 text-gray-500 hover:text-gray-800 transition-colors"
+    >
+      <HiArrowLeft className="w-5 h-5" />
+      {children}
+    </Link>
+  );
+}
