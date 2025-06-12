00547a0b48a0dd6f20c7b43c3d1250d033ff3282
Quinten <quintenclaes7@gmail.com>
2025-06-12 18:00:06 +0200

    fix: update link state in Vans page to include search parameters for better navigation

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -11,27 +11,27 @@ export default function VanDetail() {
   return (
     <div>
       {van ? (
-        <div className="flex flex-col lg:flex-row items-center justify-center p-6 gap-8">
-          <BackLink to=".." relative="path">
-            Back to all vans
-          </BackLink>
-          <img
-            className="rounded-xl w-full max-w-md"
-            src={van.imageUrl}
-            alt={van.name}
-          />
-          <div>
-            <h2 className="text-3xl font-bold mt-4 md:text-6xl mb-6">
-              {van.name}
-            </h2>
-            <Tag isActive={true}>{van.type}</Tag>
-            <p className="text-2xl text-gray-700 mt-4">${van.price}/day</p>
-            <p className="text-lg text-gray-500 mt-2">{van.description}</p>
-            <button className="mt-4 px-6 py-2 bg-orange-500 text-white rounded-lg hover:bg-orange-600">
-              Rent this van
-            </button>
+        <>
+          <BackLink to=".." path="relative">Go back </BackLink>
+          <div className="flex flex-col lg:flex-row items-center justify-center p-6 gap-8">
+            <img
+              className="rounded-xl w-full max-w-md"
+              src={van.imageUrl}
+              alt={van.name}
+            />
+            <div>
+              <h2 className="text-3xl font-bold mt-4 md:text-6xl mb-6">
+                {van.name}
+              </h2>
+              <Tag isActive={true}>{van.type}</Tag>
+              <p className="text-2xl text-gray-700 mt-4">${van.price}/day</p>
+              <p className="text-lg text-gray-500 mt-2">{van.description}</p>
+              <button className="mt-4 px-6 py-2 bg-orange-500 text-white rounded-lg hover:bg-orange-600">
+                Rent this van
+              </button>
+            </div>
           </div>
-        </div>
+        </>
       ) : (
         <>
           {isLoading && (
